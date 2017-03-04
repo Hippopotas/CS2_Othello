3618470789127496c871831a720f798dab171188
cs2student <charleslien97@gmail.com>
2017-03-04 00:18:36 -0800

    player now plays random moves

--- player.cpp
+++ player.cpp
@@ -9,18 +9,61 @@ Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
 
-    /*
-     * TODO: Do any initialization you need to do here (setting up the board,
-     * precalculating things, etc.) However, remember that you will only have
-     * 30 seconds.
-     */
+    board = new Board();
+    empty_spaces = 60;
+
+
+    this->side = side;
+    if (side == WHITE) {
+        opponentSide = BLACK;
+    } else {
+        opponentSide = WHITE;
+    }
+
+    adjacent = new std::vector<Move>();
+    adjacent->push_back(Move(2, 2));
+    adjacent->push_back(Move(2, 3));
+    adjacent->push_back(Move(2, 4));
+    adjacent->push_back(Move(2, 5));
+    adjacent->push_back(Move(3, 5));
+    adjacent->push_back(Move(4, 5));
+    adjacent->push_back(Move(5, 5));
+    adjacent->push_back(Move(5, 4));
+    adjacent->push_back(Move(5, 3));
+    adjacent->push_back(Move(5, 2));
+    adjacent->push_back(Move(4, 2));
+    adjacent->push_back(Move(3, 2));
+
+    occupied = new std::vector<Move>();
+    occupied->push_back(Move(3, 3));
+    occupied->push_back(Move(3, 4));
+    occupied->push_back(Move(4, 3));
+    occupied->push_back(Move(4, 4));
 }
 
 /*
  * Destructor for the player.
  */
 Player::~Player() {
+    delete adjacent;
+    delete occupied;
+    delete board;
 }
+
+int index_of(std::vector<Move> * moves, int x, int y) {
+    for (unsigned int i = 0; i < moves->size(); i++) {
+        if ((*moves)[i].x == x && (*moves)[i].y == y) {
+            return int(i);
+        }
+    }
+    return -1;
+}
+
+
+
+
+
+
 
 /*
  * Compute the next move given the opponent's last move. Your AI is
@@ -36,9 +79,64 @@ Player::~Player() {
  * return nullptr.
  */
 Move *Player::doMove(Move *opponentsMove, int msLeft) {
-    /*
-     * TODO: Implement how moves your AI should play here. You should first
-     * process the opponent's opponents move before calculating your own move
-     */
+    if (opponentsMove != nullptr) {
+        makeMoveOnBoard(opponentsMove, opponentSide);
+    }
+
+    int t_move = msLeft * 2 / empty_spaces;
+    if (msLeft == -1) {
+        t_move = -1;
+    }
+
+    Move * m = calculate_move(t_move);
+    
+    makeMoveOnBoard(m, side);
+    
+    return m;
+}
+
+Move *Player::calculate_move(int msLeft) {
+
+    if (msLeft == -1) {
+        msLeft = 60000;
+    }
+    for (unsigned int i = 0; i < adjacent->size(); i++) {
+        if (board->checkMove(&(*adjacent)[i], side)) {
+            int x = (*adjacent)[i].getX();
+            int y = (*adjacent)[i].getY();
+            Move * m = new Move(x, y);
+            return m;
+        }
+    }
     return nullptr;
+}
+
+void Player::makeMoveOnBoard(Move * m, Side side) {
+    board->doMove(m, side);
+
+    int x = m->x;
+    int y = m->y;
+    int adji = index_of(adjacent, x, y);
+    if (adji >= 0) {
+        adjacent->erase(adjacent->begin() + adji);
+    } else {
+        std::cerr << "move not in adjacent (this should never occur)" << std::endl;
+    }
+
+    for (int dx = -1; dx < 2; dx++) {
+        for (int dy = -1; dy < 2; dy++) {
+            if (dx == 0 && dy == 0) {
+                continue;
+            }
+            if (0 <= x + dx && x + dx < 8 && 0 <= y + dy && y + dy < 8) {
+                if (index_of(occupied, x + dx, y + dy) == -1 &&
+                        index_of(adjacent, x + dx, y + dy) == -1) {
+                    adjacent->push_back(Move(x + dx, y + dy));
+                }
+            }
+        }
+    }
+    occupied->push_back(Move(x, y));
+
+    empty_spaces--;
 }
--- player.hpp
+++ player.hpp
@@ -4,15 +4,28 @@
 #include <iostream>
 #include "common.hpp"
 #include "board.hpp"
+#include <vector>
+
 using namespace std;
 
 class Player {
 
 public:
+    Board *board;
+
+    int empty_spaces;
+    std::vector<Move> * adjacent;
+    std::vector<Move> * occupied;
+
+    Side side;
+    Side opponentSide;
+
     Player(Side side);
     ~Player();
 
     Move *doMove(Move *opponentsMove, int msLeft);
+    Move *calculate_move(int ms_for_move);
+    void makeMoveOnBoard(Move * move, Side side);
 
     // Flag to tell if the player is running within the test_minimax context
     bool testingMinimax;
